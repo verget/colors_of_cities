76c27c6dd7d653e3594ab6e14f4beb40bf99ac0b
verget <verget@qip.ru>
2014-06-15 15:51:14 +0400

    [#4] feature autorithe

--- app/authorizers/application_authorizer.rb
+++ app/authorizers/application_authorizer.rb
@@ -1,7 +1,7 @@
 # Other authorizers should subclass this one
 class ApplicationAuthorizer < Authority::Authorizer
 
-  # Any class method from Authority::Authorizer that isn't overridden
+  # Any class method from. Authority::Authorizer that isn't overridden
   # will call its authorizer's default method.
   #
   # @param [Symbol] adjective; example: `:creatable`
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable  
 include Authority::UserAbilities devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :email, presence: true
